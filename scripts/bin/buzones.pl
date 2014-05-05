50cb42c578dd69c8d328ae94ac192f403d444add
Juan Luis Boya García <ntrrgc@gmail.com>
2014-05-05 14:03:33 +0200

    Made many changes in buzones.pl. Hopefully it may be more stable now.
    Instead of in-memory hashes, information is stored in persistent tables
    and erased when no longer needed.
    
    Message-ID and Subject mappings are stored for 24 hours now, which may
    fix a problem with subject lines of mails coming from distribution
    lists.

--- scripts/bin/buzones.pl
+++ scripts/bin/buzones.pl
@@ -22,178 +22,246 @@
 # *    <http://www.gnu.org/licenses/>.
 # */
 
+use feature "state";
 use Encode;
 use HTML::Entities qw(encode_entities);
-use Time::Local qw(timelocal);
-use POSIX qw(strftime);
 use DateTime::Format::ISO8601;
 
-$PATH = "/srv/mailtrack/scripts";
+our $PATH = "/srv/mailtrack/scripts";
 require("$PATH/etc/config.pl");
 require("$PATH/bin/funciones.pl");
 
-# Realizamos la conexión a la base de datos
-$dbh = DBI->connect($connectionInfo,$userid,$passwd, {mysql_enable_utf8=>1}) or die "Can't connect to the database.\n";
+# Connect to the database
+our $dbh = DBI->connect($connectionInfo,$userid,$passwd, {mysql_enable_utf8=>1}) 
+        or die "Can't connect to the database.\n";
 
-my %mapa=();
-my %meses = ('Jan'=>0,'Feb'=>1,'Mar'=>2,'Apr'=>3,'May'=>4,'Jun'=>5,'Jul'=>6,'Aug'=>7,'Sep'=>8,'Oct'=>9,'Nov'=>10,'Dec'=>11);
+# label_info stores some information associated to Postfix labels:
+#  - mid
+#  - from
+#  - subject
+#  - expires_on (timestamp)
+# The objects are removed once processing for that label has ended or at the
+# end of the script if they are old enough (1 hour).
+our %label_info = ();
+$dbh->do("CREATE TABLE IF NOT EXISTS
+label_info_store (
+        `label` CHAR(16) PRIMARY KEY,
+        `mid` VARCHAR(255),
+        `from` VARCHAR(255),
+        `subject` VARCHAR(255),
+        `expires_on` INTEGER);");
+
+# mid_info stores some information associated with message-id's:
+#  - subject
+#  - expires_on (timestamp)
+# The objects are removed only when they reach the expiration time (24 hours).
+# This object allows us to retrieve the subject for lists' messages, which
+# appears only once, when the mail is received in the lists machine, but not
+# in deliveries.
+$dbh->do("CREATE TABLE IF NOT EXISTS
+mid_info_store (
+        `mid` VARCHAR(255) PRIMARY KEY,
+        `subject` VARCHAR(255),
+        `expires_on` INTEGER);");
+
+our %sth_store_get = ();
+our %sth_store_set = ();
+our %fields_by_index = (
+        'label' => ['mid', 'from', 'subject'],
+        'mid' => ['subject'],
+);
+while ((my $index, my $fields) = each(%fields_by_index)) {
+        my $table = $index . "_info_store";
+        foreach my $field (@$fields) {
+                $sth_store_get{$index}->{$field} = $dbh->prepare("
+                        SELECT (`$field`)
+                        FROM $table
+                        WHERE `$index` = ?");
+                $sth_store_set{$index}->{$field} = $dbh->prepare("
+                        INSERT INTO $table
+                        (`$index`, `$field`, expires_on)
+                        VALUES (?, ?, ?)
+                        ON DUPLICATE KEY
+                        UPDATE `$field` = ?, `expires_on` = ?;");
+        }
+}
+
+sub get_label_info {
+        my ($label, $field) = @_;
+
+        my $sth = $sth_store_get{"label"}->{$field} or die "Unknown field: $field";
+        $sth->execute($label);
+
+        my $value;
+        if ($sth->rows) {
+                my @row = $sth->fetchrow_array();
+                $value = $row[0];
+        }
+        
+        $sth->finish();
+        return $value;
+}
+sub set_label_info {
+        my ($label, $field, $value) = @_;
+        my $expires_on = DateTime->now()->epoch() + 3600;
+
+        my $sth = $sth_store_set{"label"}->{$field} or die "Unknown field: $field";
+        $sth->execute($label, $value, $expires_on, $value, $expires_on);
+}
+sub remove_label_info {
+        my ($label,) = @_;
+
+        state $sth = $dbh->prepare("
+                DELETE FROM label_info_store
+                WHERE `label` = ?;");
+
+        $sth->execute($label);
+}
+
+sub get_mid_info {
+        my ($mid, $field) = @_;
+
+        my $sth = $sth_store_get{"mid"}->{$field} or die "Unknown field: $field";
+        $sth->execute($label);
+
+        my $value;
+        if ($sth->rows) {
+                my @row = $sth->fetchrow_array();
+                $value = $row[0];
+        }
+        
+        $sth->finish();
+        return $value;
+}
+sub set_mid_info {
+        my ($mid, $field, $value) = @_;
+        my $expires_on = DateTime->now()->epoch() + 24 * 3600;
+
+        my $sth = $sth_store_set{"mid"}->{$field} or die "Unknown field: $field";
+        $sth->execute($mid, $value, $expires_on, $value, $expires_on);
+}
+
+sub clean_storage {
+        my $now = DateTime->now()->epoch();
+
+        $dbh->prepare("DELETE FROM label_info_store
+                       WHERE `expires_on` < ?")->execute($now);
+
+        $dbh->prepare("DELETE FROM mid_info_store
+                      WHERE `expires_on` < ?")->execute($now);
+}
 
 open(IN,"$PATH/log/temp") or die("No puedo abrir el fichero temp!\n");
 
 while (<IN>) {
-	if($_=~/(.+) postfix\/cleanup\[(.+)\]: (.+): message-id=<(.+)>/){
-		my $etiqueta = $3;
+	if($_=~/(.+) postfix\/cleanup\[(.+)\]: (.+): message-id=(.+)/){
+		my $label = $3;
 		my $mid = $4;
 
-		my $key = "$etiqueta-mid";
-		$mapa{$key} = $mid;
+                set_label_info($label, "mid", $mid);
 
-		my $key = "$mid-eti";
-		$mapa{$key} = $etiqueta;
+                # If we knew the subject for this message, associate it with its
+                # message-id.
+                if (my $subject = get_label_info($label, "subject")) {
+                        set_mid_info($mid, "subject", $subject);
+                }
 	}
 	elsif($_=~/(.+) postfix\/cleanup\[(.+)\]: (.+): info: header subject/i){
-		my $key = "$3-asunto";
-		# Algunos mensajes tienen varias líneas Subject (por ejemplo, las devoluciones).
-		# Nos quedamos con la primera.
-		if (!exists $mapa{$key}) {
-			$mapa{$key} = obtener_asunto($_);
-		}
+                my $label = $3;
+
+                # Some messages have several Subject lines (e.g. mail returned to sender)
+                # We choose the first subject.
+                if (!get_label_info($label, "subject")) {
+                        my $subject = obtener_asunto($_);
+                        set_label_info($label, "subject", $subject);
+
+                        # We mantain a hash mid -> subject in order to be able to recover
+                        # message subjects in list deliveries.
+                        #
+                        # Postfix logs the subject when the mail arrives to the mailing
+                        # lists machine, but not when it is delivered to its recipients.
+                        if (my $mid = get_label_info($label, "mid")) {
+                                set_mid_info($mid, "subject", $subject);
+                        }
+                }
 	}
 	elsif($_=~/(.+) postfix\/qmgr\[(.+)\]: (.+): from=<(.+)>/){
-		my $key = "$3-from";
-		$mapa{$key} = $4;
-	} #elseif
-#	elsif($_=~/(.+) dovecot: lda\((.+)\): sieve: msgid=<(.+)>: sent vacation response to <(.+)>/){
-#	} #elseif
-#       elsif($_=~/(.+) postfix/smtpd(.+): NOQUEUE: reject: RCPT from unknown[192.168.3.12]: 550 5.1.1 <bellidomgb@us.es>: Recipient address rejected: User unknown in virtual mailbox table; from=<(.+)> to=<(.+)>/){
-#       } #elseif
-#       elsif($_=~/(.+) dovecot: lda\((.+)\): sieve: msgid=<(.+)>: sent vacation response to <(.+)>/){
-#       } #elseif
-#       elsif($_=~/(.+) dovecot: lda(.+): Error: sieve: msgid=<(.+)>: failed to store into mailbox 'INBOX': Quota exceeded (mailbox for user is full)/){
-#       } #elseif
-	elsif($_=~/(.+) (.+) dovecot: lda\((.+)\): sieve: msgid=<(.+)>: stored mail into mailbox '(.+)'/){
-		my $mid = $4;
-		my $usuario = $3;
-		$usuario =~s/\@(.+)//;
-		my $carpeta = $5;
-
-		my $info = "- Dovecot (sieve-manager) informó: Correo guardado en la carpeta \"$carpeta\" del usuario <i>$usuario</i>";
-		my $key = "$mid-$usuario";
-		$mapa{$key} = $info;		
-
-	} #elseif
-	elsif($_=~/(.+) (.+) postfix.+\[(.+)\]: (.+): to=<(.+?)>(.+) status=(.+) \((.+)\)/){
-		my $fecha = DateTime::Format::ISO8601->parse_datetime($1)->epoch();
-		$maquina = $2;
-		my $etiqueta = $4;
-		my $mid = $mapa{"$4-mid"};
-		my $from = limpia_lavadora($mapa{"$4-from"});
-                if ($from eq "") {
+                my $label = "$3";
+                set_label_info($label, "from", $4);
+	} 
+	elsif($_=~/(.+) (.+) postfix.+\[.+\]: (.+): to=<(.+?)>(.+) status=(.+) \((.+)\)/){
+		my $date = DateTime::Format::ISO8601->parse_datetime($1)->epoch();
+		my $machine = $2;
+		my $label = $3;
+		my $mid = get_label_info($label, "mid");
+                if (!$mid) {
+                        state $already_reported_labels = {};
+                        if (!defined $already_reported_labels->{$label}) {
+                                print("Warning: No message-id recorded for label $label. Skipping...\n");
+                                $already_reported_labels->{$label} = 1;
+                        }
+                        next;
+                }
+		my $from = limpia_lavadora(get_label_info($label, "from"));
+                if (!$from) {
+                        # Placeholder name if from is absent
 			$from = "Sistema de correo";
                 }
-		my $to = $5;
-		my $redireccion = $6;
-		my $nuevo_destino = ""; #Guardamos en esta variable la redirección si existe
-		my $status = $7;
-		my $desc = "- Postfix informó del siguiente estado: $7 ($8)";
+		my $to = $4;
+		my $redir_text = $5;
+		my $alias_email = "";
+		my $status = $6;
+		my $description = "- Postfix informó del siguiente estado: $7 ($8)";
 		
-		#Obtenemos el mensaje de la carpeta destino del manejador sieve
-		my $nombre_usuario = lc($to);
-		$nombre_usuario =~s/@(.+)//;
+		my $aditional_info = "";
 
-		my $key = "$mid-$nombre_usuario";
-		my $adicional = $mapa{$key};
-		my $query = "";
-
-		$estado = $ENTREGADO_LOCAL;
-		#Comprobamos si se está utilizando una redirección de correo
-		if($redireccion=~/ orig_to=<(.+?)>/){
-			$nuevo_destino = $to;
+		$status = $ENTREGADO_LOCAL;
+		# Check if a mail alias is in use
+		if($redir_text =~ / orig_to=<(.+?)>/){
+			$alias_email = $to;
 			$to = $1;
-			$estado = $ENTREGADO_REDIRECCION;
-			$adicional = "- Postfix/smtp informó: El destinatario utiliza una redirección desde la cuenta <i>".
-                                     encode_entities($to)."</i> a la cuenta <i>".encode_entities($nuevo_destino)."</i>.";
+			$status = $ENTREGADO_REDIRECCION;
+			$aditional_info = "- Postfix/smtp informó: El destinatario utiliza una redirección desde la cuenta <i>".
+                                          encode_entities($to)."</i> a la cuenta <i>".encode_entities($alias_email)."</i>.";
 			
 			 #Incrementamos el número de correos redirigidos
                         $query = "UPDATE estadisticas SET redirecciones = redirecciones + 1;";
                         $sth = $dbh->prepare($query);
                         $sth->execute();
-
 		}
                 if($status ne 'sent')
                 {
-                        $estado = $ERROR_BUZONES;
+                        $status = $ERROR_BUZONES;
                 }
 
-		#Miramos si ya existia el mensaje en la BBDD
-		$mid =~s/AAAAAA==@(.+)//g; # Esto lo hacemos pq postfix añade este sufijo a ciertos message_id
-		my $midcadena = "%".$mid."%";
-		$query = "SELECT message_id,estado FROM mensajes WHERE message_id LIKE ? AND mto=?;";
-		$sth = $dbh->prepare($query);
-		$sth->execute($midcadena, $to);
-		my @row;
+                my $subject = get_label_info($label, "subject") or get_mid_info($mid, "subject") or "";
 
-#Sino se encontró ningún mensaje, vamos a buscar uno parecido teniendo en cuenta 
-# que postfix, modifica algunos message_id a su antojo
-                if (! $sth->rows )
-                {
-			#No encontró el mensaje, hacemos un cruce temporal
-                        $fechainf = $fecha - 500;
-                        $fechasup = $fecha + 100;
-                        $query = "SELECT message_id,estado FROM mensajes ".
-                                 "WHERE mfrom = ? AND mto = ? AND fecha > ? AND fecha < ? ".
-                                 "AND estado < 300 ORDER BY fecha DESC;";
-		#print "$query\n";
-                        $sth = $dbh->prepare($query);
-                        $sth->execute($from, $to, $fechainf, $fechasup);
-			@row = $sth->fetchrow_array(  );
-		}
+                my $subject_db;
+                if ($subject =~ /\S/) {
+                        $subject_db = encode_entities($subject);
+                } else {
+                        $subject_db = "<i>(sin asunto)</i>";
+                }
 
-		if ( $sth->rows )
-		{
-			@row = $sth->fetchrow_array(  );
+                $query = "INSERT INTO mensajes (message_id,mfrom,mto,redirect,asunto,estado,fecha) ".
+                         "VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE estado = ?;";
 
-			my $estado_actual = $row[1];
-			$mid = $row[0];
-			$estado = calcula_estado($estado,$estado_actual);
-			
-			$query = "UPDATE mensajes SET estado = ?, redirect = ? WHERE mto = ? AND message_id = ?;";
-			$sth = $dbh->prepare($query);
-			$sth->execute($estado, $nuevo_destino, $to, $mid);
-		}
-		else
-		{
-			#No existía, lo añadimos
-			if (exists $mapa{"$etiqueta-asunto"}) {
-				$asunto = $mapa{"$etiqueta-asunto"};
-				if ($asunto =~ /\S/) {
-					$asunto = encode_entities($asunto);
-				} else {
-					$asunto = "<i>(sin asunto)</i>";
-				}
-			} else {
-				$asunto = "<i>(sin asunto)</i>";
-			}
-			$query = "INSERT INTO mensajes (message_id,mfrom,mto,redirect,asunto,estado,fecha) ".
-                                 "VALUES (?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE estado = ?;";
+                $sth = $dbh->prepare($query);
+                $sth->execute($mid, $from, $to, $alias_email, $subject_db, $status, $date, $status);
 
-			$sth = $dbh->prepare($query);
-			$sth->execute($mid, $from, $to, $nuevo_destino, $asunto, $estado, $fecha, $estado);
-
-                        #Incrementamos el número de correos procesados
-                        $query = "UPDATE estadisticas SET procesados = procesados + 1;";
-                        $sth = $dbh->prepare($query);
-                        $sth->execute();
-		}
+                #Incrementamos el número de correos procesados
+                $query = "UPDATE estadisticas SET procesados = procesados + 1;";
+                $sth = $dbh->prepare($query);
+                $sth->execute();
 
 		#En cualquier caso, actualizamos el historial del mensaje
 		$query = "INSERT IGNORE INTO historial (message_id,estado,hto,fecha,maquina,descripcion,adicional) ".
                          "VALUES (?,?,?,?,?,?,?);";
 		$sth = $dbh->prepare($query);
-		$sth->execute($mid, $estado, $to, $fecha, $maquina, $desc, $adicional);
+		$sth->execute($mid, $status, $to, $date, $machine, $description, $aditional_info);
 
 		#Incrementamos el numero de correos para dominios propios si el estado fue entregado local
-		if ($estado eq $ENTREGADO_LOCAL)
+		if ($status eq $ENTREGADO_LOCAL)
 		{
 			#Incrementamos el número de correos redirigidos a alias o dom. virtuales
                         my $query = "UPDATE estadisticas SET interno = interno + 1;";
@@ -201,12 +269,16 @@ while (<IN>) {
                         $sth->execute();
 		}
 
-	} #elseif
+	}
+        elsif($_=~/(.+) postfix\/qmgr\[(.+)\]: (.+): removed/){
+                my $label = $3;
 
-}	#while principal
+                # Once the message is processed we can remove it from the storage
+                # (Note we keep the mid -> subject association for a longer time)
+                remove_label_info($label);
+        }
+}
 
-$dbh->disconnect;
-undef %mapa;
-undef %meses;
+clean_storage();
+$dbh->disconnect();
 close(IN);
-exit;
