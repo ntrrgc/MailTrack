2061beb43678a0d6d78f55c0c6025ca21639a303
Juan Luis Boya García <ntrrgc@gmail.com>
2014-05-07 13:58:39 +0200

    Use proper locking

--- scripts/bin/actualiza.sh
+++ scripts/bin/actualiza.sh
@@ -24,117 +24,96 @@
 
 RUTA="/srv/mailtrack/scripts"
 RUTA_LOG="/tmp/seglog"
-FLAG="/run/mailtrack/seguimiento_flag_lock"
-FLAG_BORRADO="/run/mailtrack/seguimiento_flag_delete_lock"
+LOCK_FILE="/run/mailtrack/seguimiento_flag_lock"
 
-function clear_flags {
-        rm -f "$FLAG" "$FLAG_BORRADO"
-}
+( flock -n 9 || exit
 
-if [ -f $FLAG ]; then
-	#El bloqueo esta activo por otra ejecución concurrente de indexado
-	exit
-else
-	#Si esta ejecutandose el proceso de borrado, abortamos el indexado
-	if [ -f $FLAG_BORRADO ]; then
-	        #El bloqueo esta activo por otra ejecución concurrente de borrado
-        	exit
-	fi
-
-        trap clear_flags EXIT
-
-	#Creamos el flag de bloqueo
-	touch $FLAG
-
-	date
-	echo ""
-	echo "------------------------------------------------------------------"
-	echo "   >>>>> Iniciando descarga de ficheros de logs remotos"
-	echo "------------------------------------------------------------------"
-	echo ""
+        date
+        echo ""
+        echo "------------------------------------------------------------------"
+        echo "   >>>>> Iniciando descarga de ficheros de logs remotos"
+        echo "------------------------------------------------------------------"
+        echo ""
 
         if [ ! -d "$RUTA_LOG" ]; then
-		mkdir $RUTA_LOG
+                mkdir $RUTA_LOG
         fi
 
-	#Recorrido inverso para la descarga de los logs
-	grep -v '#' $RUTA/etc/$1 | tac | while read line 
-	do
-		HOST=`echo $line |  cut -d , -f1`
-	        IP=`echo $line |  cut -d , -f2`
-	        LOG=`echo $line |  cut -d , -f3`
-	        PROGRAMA=`echo $line |  cut -d , -f4`
+        #Recorrido inverso para la descarga de los logs
+        grep -v '#' $RUTA/etc/$1 | tac | while read line 
+        do
+                HOST=`echo $line |  cut -d , -f1`
+                IP=`echo $line |  cut -d , -f2`
+                LOG=`echo $line |  cut -d , -f3`
+                PROGRAMA=`echo $line |  cut -d , -f4`
 
                 if [ "$PROGRAMA" = "adas.pl" ]; then
                         # El script de adAS no necesita descarga de logs
                         continue
                 fi
-	
-	        echo "Descargando el log $LOG de $HOST......."
-	        scp $IP:$LOG $RUTA_LOG/$HOST.actual
-		echo "Descarga completada (OK)."
-		echo ""
 
-	        if [ "$PROGRAMA" = "salida.pl" ]; then
-        	        perl $RUTA/bin/mtrack $RUTA_LOG/$HOST.actual > $RUTA_LOG/temp
-                	mv $RUTA_LOG/temp $RUTA_LOG/$HOST.actual
-	        fi
-		if [ "$PROGRAMA" = "antivirus.pl" ]; then
-	                #Comprobamos que la ultima lina no sea el resultado de spam
-        	        CONT=`tail -1 $RUTA_LOG/$HOST.actual | grep "spamd: result" | wc -l`;
-                	if [ $CONT -eq 1 ]; then
-                        	cp $RUTA_LOG/$HOST.actual $RUTA_LOG/copia
-	                        sed '$d' $RUTA_LOG/copia > $RUTA_LOG/$HOST.actual
-        	                rm $RUTA_LOG/copia
-                	fi
-	        fi
-        	if [ "$PROGRAMA" = "listas.pl" ]; then
-                	#Comprobamos que la ultima lina no sea la de from
-	                CONT=`tail -1 $RUTA_LOG/$HOST.actual | grep "from=<" | wc -l`;
-        	        if [ $CONT -eq 1 ]; then
-                	        cp $RUTA_LOG/$HOST.actual $RUTA_LOG/copia
-	                        sed '$d' $RUTA_LOG/copia > $RUTA_LOG/$HOST.actual
-        	                rm $RUTA_LOG/copia
-                	fi
-	        fi
-	done
+                echo "Descargando el log $LOG de $HOST......."
+                scp $IP:$LOG $RUTA_LOG/$HOST.actual
+                echo "Descarga completada (OK)."
+                echo ""
 
-	echo ""
-	echo "------------------------------------------------------------------"
-	echo "   >>>>> Descarga de logs completada, inicio de procesamiento"
-	echo "------------------------------------------------------------------"
-	echo ""
-	# Recorrido en orden natural para la ejecución de procesamiento de los logs
-	grep -v '#' $RUTA/etc/$1 | while read line 
-	do 
-		HOST=`echo $line |  cut -d , -f1`
-	        PROGRAMA=`echo $line |  cut -d , -f4`
-	
-		if [ ! -f $RUTA_LOG/$HOST ]
-		then
-		    echo "" > $RUTA_LOG/$HOST;
-		fi
-        
+                if [ "$PROGRAMA" = "salida.pl" ]; then
+                        perl $RUTA/bin/mtrack $RUTA_LOG/$HOST.actual > $RUTA_LOG/temp
+                        mv $RUTA_LOG/temp $RUTA_LOG/$HOST.actual
+                fi
+                if [ "$PROGRAMA" = "antivirus.pl" ]; then
+                        #Comprobamos que la ultima lina no sea el resultado de spam
+                        CONT=`tail -1 $RUTA_LOG/$HOST.actual | grep "spamd: result" | wc -l`;
+                        if [ $CONT -eq 1 ]; then
+                                cp $RUTA_LOG/$HOST.actual $RUTA_LOG/copia
+                                sed '$d' $RUTA_LOG/copia > $RUTA_LOG/$HOST.actual
+                                rm $RUTA_LOG/copia
+                        fi
+                fi
+                if [ "$PROGRAMA" = "listas.pl" ]; then
+                        #Comprobamos que la ultima lina no sea la de from
+                        CONT=`tail -1 $RUTA_LOG/$HOST.actual | grep "from=<" | wc -l`;
+                        if [ $CONT -eq 1 ]; then
+                                cp $RUTA_LOG/$HOST.actual $RUTA_LOG/copia
+                                sed '$d' $RUTA_LOG/copia > $RUTA_LOG/$HOST.actual
+                                rm $RUTA_LOG/copia
+                        fi
+                fi
+        done
+
+        echo ""
+        echo "------------------------------------------------------------------"
+        echo "   >>>>> Descarga de logs completada, inicio de procesamiento"
+        echo "------------------------------------------------------------------"
+        echo ""
+        # Recorrido en orden natural para la ejecución de procesamiento de los logs
+        grep -v '#' $RUTA/etc/$1 | while read line 
+        do 
+                HOST=`echo $line |  cut -d , -f1`
+                PROGRAMA=`echo $line |  cut -d , -f4`
+
+                if [ ! -f $RUTA_LOG/$HOST ]
+                then
+                    echo "" > $RUTA_LOG/$HOST;
+                fi
+
                 if  [ "$PROGRAMA" != "adas.pl" ]; then
                         diff $RUTA_LOG/$HOST.actual $RUTA_LOG/$HOST | grep "^<" | sed -e 's/^< //g' > $RUTA/log/temp
                 fi
-	
-		echo ""
-		echo "Ejecutando $PROGRAMA sobre $RUTA_LOG/$HOST";
-	
-		time perl $RUTA/bin/$PROGRAMA $RUTA
-		echo "Ejecución completada (OK)."
-		echo ""
-	
+
+                echo ""
+                echo "Ejecutando $PROGRAMA sobre $RUTA_LOG/$HOST";
+
+                time perl $RUTA/bin/$PROGRAMA $RUTA
+                echo "Ejecución completada (OK)."
+                echo ""
+
                 if  [ "$PROGRAMA" != "adas.pl" ]; then
                         mv $RUTA_LOG/$HOST.actual $RUTA_LOG/$HOST
                         rm $RUTA/log/temp
                 fi
-	done
+        done
 
-	date
+        date
 
-	#Liberamos el flag de bloqueo
-	rm $FLAG
-fi
-
+) 9> "$LOCK_FILE"
